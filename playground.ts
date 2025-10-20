07a85e55352969410d3481c18cbdc122629bd36c
unknown <nathaniel2048@gmail.com>
2025-10-21 07:55:01 +0800

    Add more examples in playground

--- playground.ts
+++ playground.ts
@@ -1,3 +1,204 @@
+// type Index<T> = Exclude<keyof T, keyof any[]>;
+
+// type DisjointArray<T extends readonly any[]> = Index<T> extends infer K // declare K
+//   ? (
+//       (
+//         K extends Index<T> // distributive conditional type to partition Index<T>
+//           ? (_: T[K] extends (infer I1)[] ? I1 : never) => void // infer I unionize the types inside each array
+//           : never
+//       ) extends (_: infer I2) => void // function argument intersect the distributive conditional type
+//         ? I2
+//         : never
+//     ) extends never // intersection is empty
+//     ? true
+//     : false
+//   : never;
+
+// type D = DisjointArray<[number, number | string, string]>;
+// type E = DisjointArray<["number", "string"]>;
+// type F = DisjointArray<["string", string]>;
+
+// type G = DisjointArray<[["number"], ["string"]]>;
+// type H = DisjointArray<[["number"], ["number", "string"]]>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// type Index<T> = Exclude<keyof T, keyof any[]>;
+
+// type Disjoint<T extends readonly any[]> = Index<T> extends infer K // declare K
+//   ? (
+//       (K extends Index<T> ? (_: T[K]) => void : never) extends (
+//         _: infer I
+//       ) => void
+//         ? I
+//         : never
+//     ) extends never
+//     ? true
+//     : false
+//   : never;
+
+// type D = Disjoint<[number, number | string, string]>;
+// type E = Disjoint<["number", "string"]>;
+// type F = Disjoint<["string", string]>;
+
+// type G = Disjoint<[["number"], ["string"]]>;
+// type H = Disjoint<[["number"], ["number", "string"]]>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// type Intersect<T, K> = (K extends keyof T ? (_: T[K]) => void : never) extends (
+//   _: infer I
+// ) => void
+//   ? I
+//   : never;
+
+// type Index<T extends readonly any[]> = Exclude<keyof T, keyof any[]>;
+
+// type Intersection<T extends readonly any[]> = Intersect<T, Index<T>>;
+
+// type A = Intersection<[number, number | string]>;
+// type B = Intersection<["number", "number" | "string"]>;
+// type C = Intersection<["string", string]>;
+
+// type Disjoint<T extends readonly any[]> = Intersection<T> extends never
+//   ? true
+//   : false;
+
+// type D = Disjoint<[number, number | string, string]>;
+// type E = Disjoint<["number", "string"]>;
+// type F = Disjoint<["string", string]>;
+
+// // type DisjointArray<T extends readonly any[]> = Disjoint<{
+// //   readonly [K in keyof T]: T[K] extends (infer I)[] ? I : never;
+// // }>;
+
+// type DisjointArray<T extends readonly any[]> = Disjoint<{
+//   [K in keyof T]: T[K] extends (infer I)[] ? I : never;
+// }>;
+
+// type G = DisjointArray<[["number"], ["string"]]>;
+// type H = DisjointArray<[["number"], ["number", "string"]]>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// type Intersect<T, K> = (K extends keyof T ? (_: T[K]) => void : never) extends (
+//   _: infer I
+// ) => void
+//   ? I
+//   : never;
+
+// type Index<T> = Exclude<keyof T, keyof any[]>;
+
+// type Intersection<T> = Intersect<T, Index<T>>;
+
+// type A = Intersection<[number, number | string]>;
+// type B = Intersection<["number", "number" | "string"]>;
+// type C = Intersection<["string", string]>;
+
+// type Disjoint<T> = Intersection<T> extends never ? true : false;
+
+// type D = Disjoint<[number, number | string, string]>;
+// type E = Disjoint<["number", "string"]>;
+// type F = Disjoint<["string", string]>;
+
+// type DisjointArray<T> = Disjoint<{
+//   [K in Index<T>]: T[K] extends (infer I)[] ? I : never;
+// }>;
+
+// type G = DisjointArray<[["number"], ["string"]]>;
+// type H = DisjointArray<[["number"], ["number", "string"]]>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// type Index<T extends readonly any[]> = Extract<keyof T, `${number}` | number>;
+
+// type Index<T extends readonly any[]> = Exclude<keyof T, keyof any[]>;
+
+// type Intersection<T extends readonly any[]> = Intersect<T, Index<T>>;
+
+// type Disjoint<T extends readonly any[]> = Intersection<T> extends never
+//   ? true
+//   : false;
+
+// type DisjointArray<T extends readonly any[]> = Disjoint<{
+//   [K in Index<T>]: T[K] extends (infer I)[] ? I : never;
+// }>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // type DisjointArray<T extends readonly any[]> = (
+// //   (T extends any ? (_: T[keyof T]) => void : never) extends (_: infer I) => void
+// //     ? I
+// //     : never
+// // ) extends never
+// //   ? true
+// //   : false;
+
+// type DisjointArray<T extends readonly any[]> = (
+//   (T[number] extends any ? (_: T[number]) => void : never) extends (
+//     _: infer I
+//   ) => void
+//     ? I
+//     : never
+// ) extends never
+//   ? true
+//   : false;
+
+// type D = DisjointArray<[number, number | string, string]>;
+// type E = DisjointArray<["number", "string"]>;
+// type F = DisjointArray<["string", string]>;
+
+// type G = DisjointArray<[["number"], ["string"]]>;
+// type H = DisjointArray<[["number"], ["number", "string"]]>;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// type Disjoint<Type> = Type extends (infer Intersection)[]
+//   ? Intersection extends never
+//     ? true
+//     : false
+//   : never;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // 1) canonical union -> intersection
+// type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+//   k: infer I
+// ) => void
+//   ? I
+//   : never;
+
+// // 2) intersection of the tuple/array element types
+// type ElementsIntersection<T extends readonly any[]> = UnionToIntersection<
+//   T[number]
+// >;
+
+// // 3) final test: true if X extends the intersection of elements of T
+// type ExtendsElementsIntersection<X, T extends readonly any[]> =
+//   // if T has no elements, T[number] is `never` -> treat as false (or adjust as you want)
+//   [T[number]] extends [never]
+//     ? false
+//     : X extends ElementsIntersection<T>
+//     ? true
+//     : false;
+
+// type A = [{ a: number }, { b: string }];
+
+// type I = ElementsIntersection<A>; // { a: number } & { b: string }
+
+// type Check1 = ExtendsElementsIntersection<{ a: number; b: string }, A>; // true
+// type Check2 = ExtendsElementsIntersection<{ a: number }, A>; // false
+// type Check3 = ExtendsElementsIntersection<
+//   { a: number; b: string; c: boolean },
+//   A
+// >; // true
+
+// // Empty array case:
+// type Empty = [];
+// type CheckEmpty = ExtendsElementsIntersection<{}, Empty>; // false
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // type ValueStringUnion = "boolean" | "number" | "string";
 
 // type ColumnInput<
